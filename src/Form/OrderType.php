4e075dc0c32f3a2a58360d422461dc72306c429a
Amisp <aneero.bat@gmail.com>
2026-05-11 12:06:10 +0200

    Added THE module for ordering

---
+++ src/Form/OrderType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Menu;
+use App\Entity\Order;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+            ])
+            ->add('deliveryDate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('deliveryTime', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('peopleCount', IntegerType::class, [
+                'label' => 'Nombre de personnes',
+            ])
+            ->add('distanceKm', NumberType::class, [
+                'label' => 'Distance (km)',
+                'input' => 'string',
+            ])
+            ->add('menu', EntityType::class, [
+                'class' => Menu::class,
+                'choice_label' => 'title',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
