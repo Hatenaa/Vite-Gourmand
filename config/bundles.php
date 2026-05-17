e2cb443e1c83e5b9e48a8f5c4fead904bff49065
Amisp <aneero.bat@gmail.com>
2026-05-17 13:48:41 +0200

    feat: setup Webpack Encore with Bootstrap SCSS and update header layout

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
