52c517058d2ccf39c36d8dec31661e49c8ba6655
Amisp <aneero.bat@gmail.com>
2026-05-31 16:26:27 +0200

    chore: configure Webpack Encore and add frontend dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,13 @@ Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .addEntry('app', './assets/app.js')
+    .addEntry('home', './assets/js/home-menus.js')
+    .addEntry('stats', './assets/js/stats-chart.js')
+    .addEntry('menu-filters', './assets/js/menu-filters.js')
+    .addEntry('order-filters', './assets/js/order-filters.js')
     .enableSassLoader()
-    .enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
+    
 ;
 
 module.exports = Encore.getWebpackConfig();
